9bc583713ea03377bcf64a41748fd2a2645e0a63
Pawel Duda <pawel.duda91@gmail.com>
2019-09-15 09:54:16 +0200

    run mix new

---
+++ lib/bb_local_events.ex
@@ -0,0 +1,18 @@
+defmodule BbLocalEvents do
+  @moduledoc """
+  Documentation for BbLocalEvents.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BbLocalEvents.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bb_local_events_test.exs
@@ -0,0 +1,8 @@
+defmodule BbLocalEventsTest do
+  use ExUnit.Case
+  doctest BbLocalEvents
+
+  test "greets the world" do
+    assert BbLocalEvents.hello() == :world
+  end
+end
