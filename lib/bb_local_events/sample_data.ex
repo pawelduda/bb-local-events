ee4111bb2722f8d9414e6419de246c7c74d8b173
Pawel Duda <pawel.duda91@gmail.com>
2019-09-15 15:48:57 +0200

    add some sample data, fix bugs with map

--- lib/bb_local_events/sample_data.ex
+++ lib/bb_local_events/sample_data.ex
@@ -2,6 +2,30 @@ defmodule BbLocalEvents.SampleData do
   def get do
     [
       "
+      13.09.2019 godzina 22:42
+
+      Bielsko-Biała, ul. 3-go Maja na wysokości skrzyżowania z ul. Dąbrowskiego - zderzenie dwóch osobówek. Na tę chwilę brak służb. Występują utrudnienia w ruchu! Kawałek dalej jadąc w stronę Zamku Sułkowskich lewy pas zajęty przez drogowców.
+      ",
+
+      "
+      14.09.2019 godzina 17:45
+
+      Bielsko-Biała, nowy parking przy ul. Paderewskiego - młodzież też ma swoje prawa 👨‍👩‍👦‍👦😂 #takasytuacja
+      ",
+
+      "
+      15.09.2019 godzina 13:40
+
+      Międzyrzecze Górne, ul. Bielska - trwa neutralizacja plamy oleju przez straż pożarną. Ruch odbywa się wahadłowo. Trzeba zachować ostrożność, bo jest bardzo ślisko!
+      ",
+
+      "
+      15.09.2019 godzina 13:30
+
+      Przełęcz Przegibek, parking Gawry - mistrzostwa powiatu w parkowaniu bez głowy trwają. Kilkadziesiąt samochodów zablokowanych na parkingu bez możliwości wyjazdu. Ciasno także wzdłuż ulicy Górskiej, gdzie mimo zakazu stoi masa pojazdów. Nie ma się co dziwić, że ludzi ciągnie w góry, ale trzeba przy tym odrobinę pomyśleć... 😬
+      ",
+
+      "
       15.09.2019 godzina 12:11
 
       Bielsko-Biała, ul. Krakowska - w dalszym ciągu za mało znaków dla zawodowych kierowców. Zwężenie przyblokowane przez busiarza jadącego pod prąd.
@@ -37,6 +61,12 @@ defmodule BbLocalEvents.SampleData do
       14.09.2019 godzina 17:45
 
       Bielsko-Biała, nowy parking przy ul. Paderewskiego - młodzież też ma swoje prawa 👨‍👩‍👦‍👦😂 #takasytuacja
+      ",
+
+      "
+      14.09.2019 godzina 18:25
+
+      Bielsko-Biała, ul. Warszawska - wyjazd z miasta w stronę Czechowic-Dziedzic - malowanie pasów. Prawy pas zablokowany tuż za łukiem przy skrzyżowaniu z ul. Królewską. Trzeba zachować ostrożność!
       "
     ]
   end
